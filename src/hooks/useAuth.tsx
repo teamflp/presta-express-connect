e7fc98c6670abe0fb81c1692635a46e94d4239b2
teamflp@yahoo.it <teamflp@yahoo.it>
2025-04-29 19:03:16 +0200

    Mise à jour de package.json

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -6,7 +6,7 @@ export const useAuth = () => {
   const context = useContext(AuthContext);
   
   if (!context) {
-    throw new Error('useAuth doit être utilisé à l'intérieur d'un AuthProvider');
+    throw new Error("useAuth doit être utilisé à l'intérieur d'un AuthProvider");
   }
   
   return context;
