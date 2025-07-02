ea7e341c43597dd8cb4e3dc4a92e5160948ed678
teamflp@yahoo.it <teamflp@yahoo.it>
2025-07-03 00:34:25 +0200

    Mise en place des tests

--- src/testing/App.test.tsx
+++ src/testing/App.test.tsx
@@ -1,15 +1,14 @@
-
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AppWithoutRouter from './AppWithoutRouter';
 
 test('renders test products component on /testProducts route', () => {
-  window.history.pushState({}, 'Test page', '/testProducts');
-  const { getByText } = render(
-      <MemoryRouter initialEntries={['/testProducts']}>
-        <AppWithoutRouter />
-      </MemoryRouter>
-  );
+    render(
+        <MemoryRouter initialEntries={['/testProducts']}>
+            <AppWithoutRouter />
+        </MemoryRouter>
+    );
 
-  expect(getByText(/Products Information/i)).toBeInTheDocument();
+    // Ce texte est bien affiché dans le composant <TestProducts />
+    expect(screen.getByRole('heading', { name: /Test Products/i })).toBeInTheDocument();
 });
